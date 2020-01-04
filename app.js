03362a7fa9f082a07d002ed15e8f4c5188057c86
Alicia <aliviaord@alum.us.es>
2020-01-04 21:45:26 +0100

    Preventing CORS

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ global.__root   = __dirname + '/';
 
 db.connect();
 
+// Preventing CORS errors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-access-token');
+
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      return res.status(200).json({});
+  }
+  
+  next();
+});
+
 app.get('/api', function (req, res) {
   res.status(200).send('API works.');
 });
